31537c6fd6337d45ec178d5e4b72b3c602474a9c
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2024-02-06 10:12:49 -0500

    idk

---
+++ project/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div>
+
+      <nav>
+      </nav>
+    </div>
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
