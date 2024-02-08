248b8f4bfe10f2c8773b0c68697801af872484d2
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2024-02-08 10:14:08 -0500

    asdfhjkl

--- project/src/App.vue
+++ project/src/App.vue
@@ -4,8 +4,9 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
+    <h1>Adopt a Pet</h1>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
