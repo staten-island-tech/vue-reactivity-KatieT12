f9a30560f8e4b5aaacab31f510f902afb0cbb2e0
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2024-02-28 10:14:36 -0500

    AwDWADW

--- project/src/components/DestCard.vue
+++ project/src/components/DestCard.vue
@@ -4,43 +4,74 @@
         <img :src="product.img" alt=""/>
         <h3> Price: {{ product.price }}</h3>
         <button @click="addtoCart">Add to Cart</button>
+
+        <form @submit.prevent="addTodo">
+    <input v-model="newTodo" required placeholder="new todo">
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+  
     </div>
 </template>
 
 
 <script setup>
-// import {ref} from "vue";
-
+import {ref} from "vue";
 const props = defineProps({
     product: Object,
 });
+// give each todo a unique id
+let id = 0
 
-const DOMSelectors = {
-    container: document.querySelector(".container"),
-    removebuttons: document.querySelectorAll(".Remove"),
-    cartContent: document.querySelector(".itemsinCart"),
-    
+const newTodo = ref('')
+const todos = ref([
+  { id: id++, text: 'Learn HTML' },
+  { id: id++, text: 'Learn JavaScript' },
+  { id: id++, text: 'Learn Vue' }
+])
+
+function addtoCart() {
+  todos.value.push({ id: id++, text: newTodo.value })
+  newTodo.value = ''
 }
-function addtoCart(){
-     DOMSelectors.cartContent.insertAdjacentHTML(
-       "beforeend",
-        `<div class = "card">
-        <h3 class="name">Name: ${props.name}</h3>
-        <img src=${props.img}> </img>
-        <h4 class="price">Price: ${props.price}</h4>
-        <button type ="button" class="Remove" @click="removeItem">Remove Item</button>
-        <br>
-    </div>`
-     );
-};
 
-function removeItem(){
-    let removebuttons = document.querySelectorAll(".Remove")
-    removebuttons.forEach((element) => element.addEventListener('click', function(){
-    element.parentElement.remove();
-            }));
-         };
-removeItem();
+function removefromCart(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
+}
+
+
+
+// const DOMSelectors = {
+//     container: document.querySelector(".container"),
+//     removebuttons: document.querySelectorAll(".Remove"),
+//     cartContent: document.querySelector(".itemsinCart"),
+    
+// }
+// function addtoCart(){
+//      DOMSelectors.cartContent.insertAdjacentHTML(
+//        "beforeend",
+//         `<div class = "card">
+//         <h3 class="name">Name: ${props.name}</h3>
+//         <img src=${props.img}> </img>
+//         <h4 class="price">Price: ${props.price}</h4>
+//         <button type ="button" class="Remove" @click="removeItem">Remove Item</button>
+//         <br>
+//     </div>`
+//      );
+// };
+
+// function removeItem(){
+//     let removebuttons = document.querySelectorAll(".Remove")
+//     removebuttons.forEach((element) => element.addEventListener('click', function(){
+//     element.parentElement.remove();
+//             }));
+//          };
+// removeItem();
 
 
 </script>
